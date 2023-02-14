f138793a1d5ba563d5a0c31685e7775a3291b0a1
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-02-14 21:48:01 +0530

    init publish

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const rootDir = require("./util/root-path");
+const path = require("path");
+
+const app = express();
